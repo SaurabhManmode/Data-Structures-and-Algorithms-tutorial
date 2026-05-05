5dc83744370dcea244490373302a514204f03d11
Saurabh Manmode <saurabh.manmode94@gmail.com>
2026-05-06 00:53:41 +0530

    largest array

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -4,9 +4,9 @@
 :
 
 .gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
-M
-langauge-basics/src/Main.java,0\c\0cb6a56d439d6a0db8536a2026ee8c012e11ef50
 d
 4langauge-basics/src/inputoutput/InputOutputDemo.java,a\6\a66f2716286c2e3fb15b1a48fde4baac1c9f9d7c
 u
 Elangauge-basics/src/conditionalstatements/ConditionalStatementEx.java,7\c\7c5ef8ebbe8b53e821bd3ce76e0e1046421df8fe
+^
+.langauge-basics/src/arrays/LargestInArray.java,0\c\0c7ce8a2f7cceaca06e29d92594f1db5fadd8274
---
+++ langauge-basics/src/arrays/LargestInArray.java
@@ -0,0 +1,19 @@
+package arrays;
+
+public class LargestInArray {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int largest = findLargestElement(arr);
+        System.out.println(largest);
+    }
+
+    private static int findLargestElement(int[] arr) {
+        int largest = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest)
+                largest = arr[i];
+        }
+        return largest;
+    }
+}
