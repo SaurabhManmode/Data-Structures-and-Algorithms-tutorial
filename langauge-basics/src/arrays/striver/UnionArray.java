a6798da98e57d74b860c11e5cac06a1a170f40d3
Saurabh Manmode <saurabh.manmode94@gmail.com>
2026-05-19 22:30:32 +0530

    union of array

--- langauge-basics/src/arrays/striver/UnionArray.java
+++ langauge-basics/src/arrays/striver/UnionArray.java
@@ -11,7 +11,46 @@ public class UnionArray {
 
         int union[] = findUnion(arr1, arr2);
 
+        Set<Integer> union2 = findUnion2(arr1, arr2);
+
         System.out.println(Arrays.toString(union));
+
+        System.out.println(union2);
+    }
+
+    private static Set<Integer> findUnion2(int[] arr1, int[] arr2) {
+
+        int i = 0;
+        int j = 0;
+
+        Set<Integer> set = new TreeSet<>();
+
+        while (i < arr1.length && j < arr2.length) {
+
+            if (arr1[i] <= arr2[j]) {
+                set.add(arr1[i]);
+                i++;
+            } else {
+                set.add(arr2[j]);
+                j++;
+            }
+        }
+
+        // Remaining elements of arr1
+        while (i < arr1.length) {
+            set.add(arr1[i]);
+            i++;
+        }
+
+        // Remaining elements of arr2
+        while (j < arr2.length) {
+            set.add(arr2[j]);
+            j++;
+        }
+
+        return set;
+
+
     }
 
     private static int[] findUnion(int[] arr1, int[] arr2) {
