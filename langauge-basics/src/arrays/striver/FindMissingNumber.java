abe6865ba2efdb7f29e68cfa78f3ab9da24fdd39
Saurabh Manmode <saurabh.manmode94@gmail.com>
2026-05-30 20:11:27 +0530

    missing number XOR solution

--- langauge-basics/src/arrays/striver/FindMissingNumber.java
+++ langauge-basics/src/arrays/striver/FindMissingNumber.java
@@ -12,6 +12,25 @@ public class FindMissingNumber {
         // optimal
         int missingNum2 = findMissingNum2(arr, n);
         System.out.println(missingNum2);
+
+        //XOR solution
+
+        int missingNum = findMissingNum3(arr, n);
+        System.out.println(missingNum);
+    }
+
+    private static int findMissingNum3(int[] arr, int n) {
+        int XOR1 = 0;
+        int XOR2 = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            XOR1 ^= arr[i];
+        }
+
+        for (int i = 1; i <= n; i++) {
+            XOR2 ^= i;
+        }
+        return XOR1 ^ XOR2;
     }
 
     private static int findMissingNum2(int[] arr, int n) {
