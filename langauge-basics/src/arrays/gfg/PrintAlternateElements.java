858c6611a0f44b80294a1273def5d5e5064c5239
Saurabh Manmode <saurabh.manmode94@gmail.com>
2026-07-05 15:41:37 +0530

    print alternate using recursion

--- langauge-basics/src/arrays/gfg/PrintAlternateElements.java
+++ langauge-basics/src/arrays/gfg/PrintAlternateElements.java
@@ -1,12 +1,34 @@
 package arrays.gfg;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrintAlternateElements {
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
 
-        for (int i = 0; i < arr.length; i += 2){
-            System.out.print(arr[i]+" ");
+        for (int i = 0; i < arr.length; i += 2) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        List<Integer> list = printAlternatenums(arr, 0);
+        System.out.println(list);
+
+    }
+
+    private static List<Integer> printAlternatenums(int[] arr, int idx) {
+        ArrayList<Integer> res = new ArrayList<>();
+        getAlternatesRec(arr, 0, res);
+        return res;
+    }
+
+    private static void getAlternatesRec(int[] arr, int idx, ArrayList<Integer> res) {
+        if (idx < arr.length) {
+            res.add(arr[idx]);
+            getAlternatesRec(arr, idx + 2, res);
         }
     }
+
+
 }
